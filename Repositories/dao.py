a43cbbfa58614249abbc89d61bd5e37071b771f8
ososdis <oscardisu2@gmail.com>
2026-05-20 10:15:48 +0100

    Initialisation des DAO

--- Repositories/dao.py
+++ Repositories/dao.py
@@ -1,20 +1,51 @@
 # Exercice 1
 ## Implémentation des DAOs des modèles DTO suivant le diagramme class_comp.puml
 
+# Avec Python, les class Interfaces peuvent être implémentées comme des classes abstraites
+# heritant de Protocol
+import sqlite3
+from typing import Protocol
+
 from Models import EnseignantDTO, EtudiantDTO, UserDTO
 
 
-class BaseDAO:
-    def get_by_id(self, id) -> object: ...
+class DAO:
+    def __init__(self, db_path: str = ":memory:"):
+        self.conn = sqlite3.connect(db_path, check_same_thread=False)
+        self._setup_db()
 
-    def get_all(self) -> list[object]: ...
+    def _setup_db(self):
+        cursor = self.conn.cursor()
+
+        # Réquêtes de création lors de l'initialisation des tables si elles n'existent pas
+        # 1. Création de la table Users sur base de la classe UserDTO
+
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, role TEXT, email TEXT, google_linked BOOLEAN)"
+        )
+
+        # 2. Création de la table Etudiant
+        # 3. Création de la table Enseignant
+        # 4. Création de la table Promotion
+        # 5. Création de la table UniteEnseignement
+        # 6. Création de la table Cours
+        # 7. Création de la table Seance
+
+        # Confirmation des réquêtes dans la transaction
+        self.conn.commit()
+
+
+class BaseDAO(Protocol):
+    def get_by_id(self, id) -> DAO: ...
+
+    def get_all(self) -> list[DAO]: ...
 
     def save(self, obj) -> None: ...
 
     def delete(self, id) -> None: ...
 
 
-class UserDAO:
+class UserDAO(DAO):
     def __init__(
         self,
     ):
@@ -31,7 +62,7 @@ class UserDAO:
     def get_by_email(self, email) -> UserDTO: ...
 
 
-class EtudiantDAO:
+class EtudiantDAO(DAO):
     def __init__(self):
         pass
 
@@ -46,7 +77,7 @@ class EtudiantDAO:
     def get_by_promotion(self, promotion_id) -> list[EtudiantDTO]: ...
 
 
-class EnseignantDAO:
+class EnseignantDAO(DAO):
     def __init__(self):
         pass
 
@@ -61,12 +92,12 @@ class EnseignantDAO:
     def get_by_ue(self, ue_id) -> list[EnseignantDTO]: ...
 
 
-class PromotionDAO:
+class PromotionDAO(DAO):
     def __init__(self) -> None:
         pass
 
 
-class EventDAO:
+class EventDAO(DAO):
     def __init__(
         self,
     ):
